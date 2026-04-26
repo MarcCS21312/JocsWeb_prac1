e1ad2a0c4c1d65b6ad50ffe4d54f4c50cd66ec58
MarcCS21312 <u1997097@campus.udg.edu>
2026-04-26 21:22:41 +0200

    Closes #17: ampliar opcions amb mode, grup i dif2

--- js/options.js
+++ js/options.js
@@ -3,11 +3,17 @@ import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 var options = function(){
     const default_options = {
         pairs: 2,
-        difficulty: 'normal'
+        difficulty: 'normal',
+        mode: '1',
+        groupSize: 2,
+        dif2: 1
     } 
 
     var pairs = $('#pairs');
     var difficulty = $('#dif');
+    var mode = $('#mode');
+    var groupSize = $('#groupSize');
+    var dif2 = $('#dif2');
     
     var savedOptions = localStorage.options && JSON.parse(localStorage.options);
     var options = Object.create(default_options);
@@ -16,9 +22,18 @@ var options = function(){
         options.pairs = savedOptions.pairs;
     if (savedOptions && savedOptions.difficulty)
         options.difficulty = savedOptions.difficulty;
+    if (savedOptions && savedOptions.mode)
+        options.mode = savedOptions.mode;
+    if (savedOptions && savedOptions.groupSize)
+        options.groupSize = savedOptions.groupSize;
+    if (savedOptions && savedOptions.dif2)
+        options.dif2 = savedOptions.dif2;
 
     pairs.val(options.pairs);
     difficulty.val(options.difficulty);
+    mode.val(options.mode);
+    groupSize.val(options.groupSize);
+    dif2.val(options.dif2);
 
     pairs.on('change', function (){
         options.pairs = pairs.val();
@@ -27,6 +42,29 @@ var options = function(){
     difficulty.on('change', function (){
         options.difficulty = difficulty.val();
     });
+
+    mode.on('change', function (){
+        options.mode = mode.val();
+        refrescarMode();
+    });
+
+    groupSize.on('change', function (){
+        options.groupSize = parseInt(groupSize.val());
+    });
+
+    dif2.on('change', function (){
+        options.dif2 = parseInt(dif2.val());
+    });
+
+    function refrescarMode(){
+        if (mode.val() === '1'){
+            $('#opcs-mode1').show();
+            $('#opcs-mode2').hide();
+        } else {
+            $('#opcs-mode1').hide();
+            $('#opcs-mode2').show();
+        }
+    }
 
     return {
         applyChanges: function(){
@@ -37,6 +75,13 @@ var options = function(){
             options.difficulty = default_options.difficulty;
             pairs.val(options.pairs);
             difficulty.val(options.difficulty);
+            options.mode = default_options.mode;
+            options.groupSize = default_options.groupSize;
+            options.dif2 = default_options.dif2;
+            mode.val(options.mode);
+            groupSize.val(options.groupSize);
+            dif2.val(options.dif2);
+            refrescarMode();
         }
     }
 }();
