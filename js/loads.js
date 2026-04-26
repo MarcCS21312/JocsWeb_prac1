7ce3d0f28c9d90a4e4ec06f56fd6ca726dd3ec3a
MarcCS21312 <u1997097@campus.udg.edu>
2026-04-26 23:09:24 +0200

    Closes #21: pantalla carregar amb llistat

---
+++ js/loads.js
@@ -0,0 +1,33 @@
+import {$} from "../library/jquery-4.0.0.slim.module.min.js";
+
+function pintar(){
+    var llista = $('#llista');
+    llista.empty();
+    var trobades = 0;
+    for (var i = 0; i < localStorage.length; i++){
+        var k = localStorage.key(i);
+        if (k.indexOf("save_") !== 0) continue;
+        trobades++;
+        var d = JSON.parse(localStorage[k]);
+        var txt = d.alias + " - " + d.data + " - " + d.score + " punts";
+        llista.append(
+            '<li><button class="carregar" data-key="' + k + '">'
+            + txt + '</button></li>'
+        );
+    }
+    if (trobades === 0){
+        llista.append('<li>Cap partida desada</li>');
+    }
+}
+
+pintar();
+
+$('#llista').on('click', '.carregar', function(){
+    var k = $(this).attr('data-key');
+    sessionStorage.load = localStorage[k];
+    window.location.assign("./game.html");
+});
+
+$('#tornar').on('click', function(){
+    window.location.assign("../");
+});
